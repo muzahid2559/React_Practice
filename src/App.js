d1e826303ac03133bbc99a9485e8e285243d1d7b
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-16 12:13:43 +0600

    React Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './App.css';
 
+
+//JSX code
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      < Person />
     </div>
   );
 }
+
+
+function Person() {
+  return (
+    <div>
+    <h1> I am person component</h1>
+  </div>
+
+  );
+}
+
 
 export default App;
