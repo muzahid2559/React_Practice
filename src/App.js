b5b63721596f9f95f87ce63939d934a4cfc30241
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-17 00:13:45 +0600

    Changing State with JS event

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import './App.css';
 import Book  from './components/book';
 
 class App extends Component {
-  
-//State
+
 state = {
   books : [
     { BookName : "1984", writer : "George Orwell" },
     { BookName : "The Da Vinci Code", writer : "Dan Brown"},
     { BookName : "The Alchemist", writer : "Paulo Coelho"}
-  ]
+  ],
+  otherProps : "I am some other prop"
 }
-  // constructor(){
-  //   super();
-  //   this.state = {};
-  // }
+
+changeBookState = () =>{
+  this.setState({
+    books : [
+      { BookName : "Nineteen Eighty-Four", writer : "George Orwell" },
+      { BookName : "The Da Vinci Code", writer : "Dan Brown"},
+      { BookName : "Metmorphosis", writer : "Franz Kafka"}
+    ]
+  });
+
+}
 
   render() {
     return (
           <div className="App">
             <h1>Book List</h1>
+            <button onClick={this.changeBookState}>Change State</button>
+
             <Book BookName = {this.state.books[0].BookName} Writer = {this.state.books[0].writer}/>
             <Book BookName = {this.state.books[1].BookName} Writer = {this.state.books[1].writer}/>
             <Book BookName = {this.state.books[2].BookName} Writer = {this.state.books[2].writer}/>
