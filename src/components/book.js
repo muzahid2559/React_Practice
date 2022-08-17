9b35c486ec32f4090593d4ccbf8ca8c3c349ad14
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-17 18:35:20 +0600

    Styling Components

--- src/components/book.js
+++ src/components/book.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../stylesheets/Book.css';
 
 const Book = props =>{
     return (
-        <div>
+        <div className="Book">
             <h3 onClick={props.change}>Book :{props.BookName}</h3>
             <h4>Writer :{props.Writer}</h4>
             <input type="text" onChange ={props.inputName} value={props.BookName}></input>
