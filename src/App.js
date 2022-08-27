e9f6dfef3d1e196839fc23dad1f96fe7bb99ad1e
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-28 00:29:43 +0600

    Setting up React Router Packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import MainComponent from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () =>{
-  return <MainComponent/>
+  return (
+    <BrowserRouter>
+      <MainComponent/>
+    </BrowserRouter>
+  );
+
 }
 
 
